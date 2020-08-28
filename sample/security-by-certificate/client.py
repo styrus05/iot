328054480fbccace887c5a1591f53f710e4c0f36
Kumar <ratannz@f8ffc226c3ee.ant.amazon.com>
2020-08-28 16:49:55 +1200

    updated client.py

--- sample/security-by-certificate/client.py
+++ sample/security-by-certificate/client.py
@@ -1,3 +1,10 @@
+# This sample code demostrates how you can use AWS IoT Policy (check policy.json)
+# to restrict access to a topic from a client device.
+# For devices registered in AWS IoT Core registry,
+# the policy grants permission to connect to AWS IoT Core with a client ID that matches a thing name,
+# and to publish to a topic whose name is equal to the certificateId of the certificate the device used
+# to authenticate itself
+
 import sys
 import ssl
 from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
@@ -7,10 +14,12 @@ import time
 # Enter your thing name from AWS IoT Core. This must match the thing name you have created because security policy attached to thing only allows connecting to the thing id.
 thingName = 'RPi-2'
 
-# Enter your certificate id. This is the topic on which client will be publishing message. Security policy attached to the thing restricts publishing to topic by CertificateId.
-certificateId = 'your-certificate-id-goes-here'
+# Enter your certificate id. This must match the id for the certificate being used for authentication.
+# You can get certificate id in AWS IoT Core where you created the certificate.
+topicName = 'enter-certificate-id-here'
 
 mqttc = AWSIoTMQTTClient(thingName)
+
 # Make sure you use the correct endpoint for the region. You can find the endpoint url from the Settings page in the left menu in AWS IoT Core.
 mqttc.configureEndpoint("data.iot.us-west-2.amazonaws.com", 8883)
 
